5dbc9c5017c283a34ca2394fa4a54c5669c62af1
CGI <CGI>
2019-04-08 11:35:10 +0530

    Stored Procedure

---
+++ storedprocedure/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace storedprocedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection(@"initial catalog=ADOdemo;data source=CGI-JAVA-055\CGI;integrated security=true;");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("p1",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Procedure executed");
+        }
+    }
+}
